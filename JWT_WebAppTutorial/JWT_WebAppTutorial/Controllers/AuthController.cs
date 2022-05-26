dcb4f1672478cb9a0caf073fe8f7e35a726ee9c7
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-26 14:47:51 -0500

    ADDED METHOS TO CREATE TOKEN

--- JWT_WebAppTutorial/JWT_WebAppTutorial/Controllers/AuthController.cs
+++ JWT_WebAppTutorial/JWT_WebAppTutorial/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace JWT_WebAppTutorial.Controllers
@@ -9,6 +12,12 @@ namespace JWT_WebAppTutorial.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(UserDto request)
@@ -33,8 +42,31 @@ namespace JWT_WebAppTutorial.Controllers
             {
                 return BadRequest("FAllo passwo");
             }
-            return Ok("Okk token");
+            string token = CreateToken(user);
+            return Ok(token);
 
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("Appsettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
