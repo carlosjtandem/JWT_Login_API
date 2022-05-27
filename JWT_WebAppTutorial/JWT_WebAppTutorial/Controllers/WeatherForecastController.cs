442e4db8a6bd08a42cd6af03838841b0fe1574e6
carlosjtandem <soycarlosjavier14@gmail.com>
2022-05-27 14:40:28 -0500

    added functionality to add a security definition in swagger

--- JWT_WebAppTutorial/JWT_WebAppTutorial/Controllers/WeatherForecastController.cs
+++ JWT_WebAppTutorial/JWT_WebAppTutorial/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWT_WebAppTutorial.Controllers
@@ -18,7 +19,7 @@ namespace JWT_WebAppTutorial.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast"),Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             int TemperatureCf = Random.Shared.Next(-20, 55);
